bdb4047781b34cba129bab660306813d6a15405b
iigorfelipe <iigorfelipe@gmail.com>
2025-06-01 15:30:21 -0300

    feat: create product listing

---
+++ src/modules/orthopedic-line/components/products-list.tsx
@@ -0,0 +1,34 @@
+import { PaginationControlled } from "./pagination";
+import { ProductCard } from "@/components/product-card";
+import { productsData } from "@/common/mocks/data";
+import { usePageParam } from "@/hooks/use-page-param";
+import { useMemo } from "react";
+
+const ITEMS_PER_PAGE = 9;
+
+export const ProductList = () => {
+  const { page, setPage } = usePageParam();
+  const totalPages = Math.ceil(productsData.length / ITEMS_PER_PAGE);
+
+  const currentItems = useMemo(() => {
+    const start = (page - 1) * ITEMS_PER_PAGE;
+    const end = page * ITEMS_PER_PAGE;
+    return productsData.slice(start, end);
+  }, [page]);
+
+  return (
+    <div className="hidden flex-col gap-[72px] lg:flex">
+      <div className="hidden flex-wrap gap-x-[30px] gap-y-[30px] sm:flex">
+        {currentItems.map((product) => (
+          <ProductCard key={product.id} product={product} page={page} />
+        ))}
+      </div>
+
+      <PaginationControlled
+        page={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
+      />
+    </div>
+  );
+};

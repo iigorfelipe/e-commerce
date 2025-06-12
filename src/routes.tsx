8edcd96a7feb7e3e880e2154ce1b1dca4e9339c5
iigorfelipe <iigorfelipe@gmail.com>
2025-06-11 21:07:37 -0300

    feat: scroll to top on route change

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,15 +3,20 @@ import { OrthopedicLine } from "./pages/orthopedic-line";
 
 import { Product } from "./pages/product ";
 import { Layout } from "./pages/layout";
+import { ScrollToTop } from "./components/scroll-to-top";
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/orthopedic-line" replace />} />
-        <Route path="orthopedic-line" element={<OrthopedicLine />} />
-        <Route path="orthopedic-line/:id" element={<Product />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/orthopedic-line" replace />} />
+          <Route path="orthopedic-line" element={<OrthopedicLine />} />
+          <Route path="orthopedic-line/:id" element={<Product />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
